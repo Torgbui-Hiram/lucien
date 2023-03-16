1e02879bd74067c288b5da7267171f8ac4b43d44
Hiram Kumado <hiramkumado@gmail.com>
2023-03-16 00:13:27 -0700

    minor

--- proj/settings.py
+++ proj/settings.py
@@ -108,12 +108,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = Path(BASE_DIR / 'staticfiles')
 STATICFILES_DIRS = [
-    Path(BASE_DIR / 'static')
+    Path(BASE_DIR / 'static'),
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
